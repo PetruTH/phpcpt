27c58db98a872dab8012d72e15115726c3feb201
PetruTH <95080404+PetruTH@users.noreply.github.com>
2022-12-25 00:40:43 +0200

    Update dbconnection.php

--- dashboard/dbconnection.php
+++ dashboard/dbconnection.php
@@ -1,12 +1,12 @@
 <?php
 
-$sname= "localhost";
+$db= "heroku_56abc638eaa6981";
 
-$uname= "root";
+$uname= "b0f6f38347a10c";
 
-$password = "";
+$password = "7c185f87";
 
-$db="utilizatori";
+$sname="eu-cdbr-west-03.cleardb.net";
 
 $conn = mysqli_connect($sname, $uname, $password, $db);
 
